ef34a9e015c31e3870f912ca79bda79ea0ec57b1
Visual <30368284+ChefKissInc@users.noreply.github.com>
2022-12-16 19:48:02 +0200

    Use cfg_if in main.rs
    Signed-off-by: Visual <30368284+ChefKissInc@users.noreply.github.com>

--- wred-client/src/main.rs
+++ wred-client/src/main.rs
@@ -15,30 +15,30 @@ mod requests;
 mod style;
 mod utils;
 
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    eframe::run_native(
-        "WhateverRed NetDbg",
-        eframe::NativeOptions {
-            #[cfg(target_os = "macos")]
-            fullsize_content: true,
-            ..Default::default()
-        },
-        Box::new(|cc| Box::new(app::WRedNetDbgApp::new(cc))),
-    );
-}
+    cfg_if! {
+        if #[cfg(target_arch = "wasm32")] {
+            console_error_panic_hook::set_once();
 
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    console_error_panic_hook::set_once();
-
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::start_web(
-            "main_canvas",
-            eframe::WebOptions::default(),
-            Box::new(|cc| Box::new(app::WRedNetDbgApp::new(cc))),
-        )
-        .await
-        .expect("failed to start eframe");
-    });
+            wasm_bindgen_futures::spawn_local(async {
+                eframe::start_web(
+                    "main_canvas",
+                    eframe::WebOptions::default(),
+                    Box::new(|cc| Box::new(app::WRedNetDbgApp::new(cc))),
+                )
+                .await
+                .expect("failed to start eframe");
+            });
+        } else {
+            eframe::run_native(
+                "WhateverRed NetDbg",
+                eframe::NativeOptions {
+                    #[cfg(target_os = "macos")]
+                    fullsize_content: true,
+                    ..Default::default()
+                },
+                Box::new(|cc| Box::new(app::WRedNetDbgApp::new(cc))),
+            );
+        }
+    }
 }
